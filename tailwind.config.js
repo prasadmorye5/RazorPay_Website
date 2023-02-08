2333b8defbbb43b2ced4c4f5e32a22c02f062bfa
Prasad <prasadmorye10@gmail.com>
2023-02-08 16:02:00 +0530

    ready product

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+    fontFamily: {
+      mullish: ["Mulish", "sans-serif"],
+    },
+    colors: {
+      deepBlue: "#02042a",
+      lightBlue: "#2b84ea",
+      lightBlue300: "#4b94ed",
+      lightBlue500: "#0b72e7",
+      greenLight: "#61cea6",
+      grayText: "#818597",
+      lightGray: "#e2e2e2",
+      grayBlue: "#344a6c",
+      deepBlueHead: "#162f56",
+      gray2: "#525a76",
+    },
+  // animation:{
+  //   "scroll-image": "scroll 20s linear infinite"
+  // },
+  // keyframes:{
+  //   scroll: {
+  //     "from": {top : 0},
+  //     "to" : {top : -500}
+  //   }
+  // }
+  },
+  },
+  plugins: [],
+}
